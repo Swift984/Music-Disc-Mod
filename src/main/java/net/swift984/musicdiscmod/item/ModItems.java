8f1277cdf8805d50e737e097505277d04a6d4a28
BuildTools <unconfigured@null.spigotmc.org>
2022-07-06 13:57:35 -0500

    The Disc

--- src/main/java/net/swift984/musicdiscmod/item/ModItems.java
+++ src/main/java/net/swift984/musicdiscmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.swift984.musicdiscmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.MusicDiscItem;
@@ -16,6 +17,66 @@ public class ModItems {
     public static final Item WATERSONG_MUSIC_DISC = registerItem("watersong_music_disc",
             new ModMusicDiscItem(7, ModSounds.WATERSONG,
                     new FabricItemSettings().group(ItemGroup.MISC).maxCount(13).rarity(Rarity.RARE).fireproof()));
+
+    public static final Item BALLROOM_MUSIC_DISC = registerItem("ballroom_music_disc",
+            new ModMusicDiscItem(7, ModSounds.BALLROOM,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE).fireproof()));
+
+    public static final Item BENNY_WORM_MUSIC_DISC = registerItem("benny_worm_music_disc",
+            new ModMusicDiscItem(7, ModSounds.BENNY_WORM,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE).food(FoodComponents.SUSPICIOUS_STEW)));
+
+    public static final Item BRYLCREAM_MUSIC_DISC = registerItem("brylcream_music_disc",
+            new ModMusicDiscItem(7, ModSounds.BRYLCREAM,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE)));
+
+    public static final Item BURNING_MEMORY_MUSIC_DISC = registerItem("burning_memory_music_disc",
+            new ModMusicDiscItem(7, ModSounds.BURNING_MEMORY,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE)));
+
+    public static final Item CANAL_ROYALE_MUSIC_DISC = registerItem("canal_royale_music_disc",
+            new ModMusicDiscItem(7, ModSounds.CANAL_ROYALE,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE).fireproof()));
+
+    public static final Item COURTYARD_MUSIC_DISC = registerItem("courtyard_music_disc",
+            new ModMusicDiscItem(7, ModSounds.COURTYARD,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE).fireproof()));
+
+    public static final Item FARAWAY_BEACH_MUSIC_DISC = registerItem("faraway_beach_music_disc",
+            new ModMusicDiscItem(7, ModSounds.FARAWAY_BEACH,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE).fireproof()));
+
+    public static final Item GHASTLY_GHOSTSHIP_MUSIC_DISC = registerItem("ghastly_ghostship_music_disc",
+            new ModMusicDiscItem(7, ModSounds.GHASTLY_GHOSTSHIP,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE).fireproof()));
+
+    public static final Item MASQUERADE_MUSIC_DISC = registerItem("masquerade_music_disc",
+            new ModMusicDiscItem(7, ModSounds.MASQUERADE,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE)));
+
+    public static final Item MY_GOLDEN_HOOK_MUSIC_DISC = registerItem("my_golden_hook_music_disc",
+            new ModMusicDiscItem(7, ModSounds.MY_GOLDEN_HOOK,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE)));
+
+    public static final Item OMFG_HELLO_MUSIC_DISC = registerItem("omfg_hello_music_disc",
+            new ModMusicDiscItem(7, ModSounds.OMFG_HELLO,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE)));
+
+    public static final Item RHYTHMIC_REEF_MUSIC_DISC = registerItem("rhythmic_reef_music_disc",
+            new ModMusicDiscItem(7, ModSounds.RHYTHMIC_REEF,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE).fireproof()));
+
+    public static final Item THIS_IS_BERK_MUSIC_DISC = registerItem("this_is_berk_music_disc",
+            new ModMusicDiscItem(7, ModSounds.THIS_IS_BERK,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE)));
+
+    public static final Item VINE_BOOM_MUSIC_DISC = registerItem("vine_boom_music_disc",
+            new ModMusicDiscItem(7, ModSounds.VINE_BOOM,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.EPIC).fireproof()));
+
+    public static final Item DOG_MUSIC_DISC = registerItem("dog_music_disc",
+            new ModMusicDiscItem(7, ModSounds.DOG,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.EPIC).fireproof()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MusicDiscMod.MOD_ID, name), item);
--- src/main/java/net/swift984/musicdiscmod/sound/ModSounds.java
+++ src/main/java/net/swift984/musicdiscmod/sound/ModSounds.java
@@ -7,7 +7,38 @@ import net.minecraft.util.registry.Registry;
 import net.swift984.musicdiscmod.MusicDiscMod;
 
 public class ModSounds {
+
     public static SoundEvent WATERSONG = registerSoundEvent("watersong");
+
+    public static SoundEvent BALLROOM = registerSoundEvent("ballroom");
+
+    public static SoundEvent BENNY_WORM = registerSoundEvent("benny_worm");
+
+    public static SoundEvent BRYLCREAM = registerSoundEvent("brylcream");
+
+    public static SoundEvent BURNING_MEMORY = registerSoundEvent("burning_memory");
+
+    public static SoundEvent CANAL_ROYALE = registerSoundEvent("canal_royale");
+
+    public static SoundEvent COURTYARD = registerSoundEvent("courtyard");
+
+    public static SoundEvent FARAWAY_BEACH = registerSoundEvent("faraway_beach");
+
+    public static SoundEvent GHASTLY_GHOSTSHIP = registerSoundEvent("ghastly_ghostship");
+
+    public static SoundEvent MASQUERADE = registerSoundEvent("masquerade");
+
+    public static SoundEvent MY_GOLDEN_HOOK = registerSoundEvent("my_golden_hook");
+
+    public static SoundEvent OMFG_HELLO = registerSoundEvent("omfg_hello");
+
+    public static SoundEvent RHYTHMIC_REEF = registerSoundEvent("rhythmic_reef");
+
+    public static SoundEvent THIS_IS_BERK = registerSoundEvent("this_is_berk");
+
+    public static SoundEvent VINE_BOOM = registerSoundEvent("vine_boom");
+
+    public static SoundEvent DOG = registerSoundEvent("dog");
 
 
     private static SoundEvent registerSoundEvent(String name) {
